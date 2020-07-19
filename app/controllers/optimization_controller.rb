fefa1e64bec8650c644112931357f5bdcd9d27e1
bianca.mouta <bianca.mouta@resultadosdigitais.com.br>
2020-07-19 18:23:30 -0300

    optimization controller and service

--- app/controllers/optimization_controller.rb
+++ app/controllers/optimization_controller.rb
@@ -1,2 +1,7 @@
-class OptimizationController < ApplicationController
+class OptimizationsController < ApplicationController
+  def index
+    @dijkstra = Optimizations.dijkstra_shortest_paths(GraphBuilder.call, GraphBuilder.edge_values)
+    @bellman_ford = Optimizations.bellman_ford_shortest_paths(GraphBuilder.call, GraphBuilder.edge_values)
+    @prim_tree = Optimizations.prim_minimum_spanning_tree(GraphBuilder.call, GraphBuilder.edge_values)
+  end
 end

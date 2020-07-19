dcc9e94cbf118fb20248e9fdacdbf7ff96e0f221
bianca.mouta <bianca.mouta@resultadosdigitais.com.br>
2020-07-19 19:08:40 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'graph#index'
+  get '/optimizations', to: 'optimizations#index'
   resources :nodes
   resources :edges
-  get '/optimizations', to: 'optimizations#index'
-  root 'graph#index'
 end

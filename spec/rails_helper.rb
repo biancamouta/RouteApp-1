b9d93eeed6737f3f1d89bd26bd43715e86bf68ce
bianca.mouta <bianca.mouta@resultadosdigitais.com.br>
2020-07-19 15:45:44 -0300

    rspec working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,9 +4,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+#require 'database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +21,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -32,76 +33,76 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-    # ShouldMatchers
-    Shoulda::Matchers.configure do |config|
-      config.integrate do |with|
-        with.test_framework :rspec
-        with.library :rails
-      end
+  # ShouldMatchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
     end
-  
-    # FactoryBot
-    config.include FactoryBot::Syntax::Methods
-  
-    # include it as shared module for all request specs in the RSpec configuration block
-    config.include RequestSpecHelper, type: :request
-    # config.before do
-    #   FactoryBot.find_definitions
-    # end
-    require 'faker'
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = false
-  
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-    end
-    config.before(:each) do
-      DatabaseCleaner.strategy = :transaction
-    end
-    config.before(:each, js: true) do
-      DatabaseCleaner.strategy = :truncation
-    end
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
-    config.before(:all) do
-      DatabaseCleaner.start
-    end
-    config.after(:all) do
-      DatabaseCleaner.clean
-    end
-  
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  end
 
-    # You can uncomment this line to turn off ActiveRecord support entirely.
-    # config.use_active_record = false
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
 
-    # RSpec Rails can automatically mix in different behaviours to your tests
-    # based on their file location, for example enabling you to call `get` and
-    # `post` in specs under `spec/controllers`.
-    #
-    # You can disable this behaviour by removing the line below, and instead
-    # explicitly tag your specs with their type, e.g.:
-    #
-    #     RSpec.describe UsersController, type: :controller do
-    #       # ...
-    #     end
-    #
-    # The different available types are documented in the features, such as in
-    # https://relishapp.com/rspec/rspec-rails/docs
-    config.infer_spec_type_from_file_location!
+  # include it as shared module for all request specs in the RSpec configuration block
+  config.include RequestSpecHelper, type: :request
+  # config.before do
+  #   FactoryBot.find_definitions
+  # end
+  require 'faker'
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # Filter lines from Rails gems in backtraces.
-    config.filter_rails_from_backtrace!
-    # arbitrary gems may also be filtered via:
-    # config.filter_gems_from_backtrace("gem name")
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # You can uncomment this line to turn off ActiveRecord support entirely.
+  # config.use_active_record = false
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, type: :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 end

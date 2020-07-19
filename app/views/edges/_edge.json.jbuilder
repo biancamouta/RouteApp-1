888e97d354f7b9b7bacde84aea3e9b25f298b829
bianca.mouta <bianca.mouta@resultadosdigitais.com.br>
2020-07-19 12:49:02 -0300

    creating edges scaffold

---
+++ app/views/edges/_edge.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! edge, :id, :name, :source, :destiny, :value, :created_at, :updated_at
+json.url edge_url(edge, format: :json)
---
+++ db/migrate/20200719154152_create_edges.rb
@@ -0,0 +1,12 @@
+class CreateEdges < ActiveRecord::Migration[6.0]
+  def change
+    create_table :edges do |t|
+      t.string :name
+      t.string :source
+      t.string :destiny
+      t.integer :value
+
+      t.timestamps
+    end
+  end
+end
